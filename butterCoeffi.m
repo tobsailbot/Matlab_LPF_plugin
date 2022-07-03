c926cdf495166de9d2d5b3d77eb92a76b5b23eba
tobsailbot <tobsailbot@gmail.com>
2022-07-03 16:35:52 -0300

    Add files via upload

--- butterCoeffi.m
+++ butterCoeffi.m
@@ -6,6 +6,12 @@ function [b, a] = butterCoeffi(f0, deltaf, fs, orden)
     fc1 = (sqrt(deltaf^2+4*f0^2)-deltaf)/2;
     fc2 = round(f0^2/fc1);
     
+    if fc1 <= 20
+        fc1 = 20;
+    elseif fc2 >= 18000;
+        fc2 = 18000;
+    end
+    
     Wn = [fc1/fN, fc2/fN]; 
     [b,a] = butter(orden,Wn); 
 end

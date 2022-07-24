9048d52f1005de41d0c69a059eaf4bb046a6cd4e
Tobias Pilarche <tobsailbot@gmail.com>
2022-07-23 23:04:30 -0300

    Delete testIf.m

--- testIf.m
+++
@@ -1,9 +0,0 @@
-classdef testIf < audioPlugin
-    methods
-        function out = process(~,in)
-            white = rand(size(in))- 0.5;
-            
-            out = in + white*0.1;
-        end
-    end 
-end
